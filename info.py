75889f4a7371bb206dccdc7db37b4f01a9d3f23a
Autoforwardbotindia <138660358+Autoforwardbotindia@users.noreply.github.com>
2023-07-14 16:37:24 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,6 +1,6 @@
 API_ID       = 29189916
 API_HASH     = "1b22031190ccfc4c1e3a394267a5526a"
-BOT_TOKEN    = ""
+BOT_TOKEN    = "6388347492:AAFebr9iEdNPXPrY0ydcTI2FJa-LoIZKlRI"
 SESSION      = "BQB184SuuYp5a6T2z9yob51YpCZhpC33GOTNlaWu8sXOwj3bYhi_Nw3H-aq3y0ApoLvloIumuaC5H6Iu4tXYHR-dKKh0tiqQVo5s8Dsg2sYRuBKRGmmjG0ocjMVlv1HzKrBrLJ_-eAxCYqshuaNIWzBwri3oQBefFXzcMOoE5uyPqXABxMlb-p8B_MUXx7tiXlFegE9s4a6SYBpeTL7-OqiJzebE9ItRxGyoxuEacwYJo3RWzVKM380YjcGyUMsdCYDE4lNCfC_PBDKdSfhHLevcSQzoQ02MgIaOnOE0HgadI-duB1j6cIhEHLXlSvPc8ehWIdq9G1_YYRypfNDALbeUAAAAAXtpeqYA"
 DATABASE_URI = "mongodb+srv://cahimab788: telegram@cluster0.a0ln81l.mongodb.net/?retryWrites=true&w=majority"
 LOG_CHANNEL  = -1001884373226
